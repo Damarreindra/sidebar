6f1c10f523148dfa550720b0f028ddf537796e2a
Damarreindra <damarihya08@gmail.com>
2022-12-24 09:41:10 +0700

    first commit

---
+++ SidebarData.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import * as IoIcons from 'react-icons/io'
+import * as FaIcons from 'react-icons/fa'
+import * as AiIcons from 'react-icons/ai'
+
+export const SidebarData = [
+    {
+        title: 'Home',
+        path:'/home',
+        icon:<AiIcons.AiFillHome/>,
+        cName:'nav-text'
+    },
+    {
+        title: 'About',
+        path:'/home',
+        icon:<AiIcons.AiFillHome/>,
+        cName:'nav-text'
+    },
+    {
+        title: 'Profile',
+        path:'/profile',
+        icon:<AiIcons.AiFillProfile/>,
+        cName:'nav-text'
+    }
+]
